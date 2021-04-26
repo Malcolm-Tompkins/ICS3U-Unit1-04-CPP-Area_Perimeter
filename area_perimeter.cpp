afeef7b2a0f6f96f05c268716097dd44f5b4d94f
Ubuntu <ubuntu@ip-172-31-27-115.ec2.internal>
2021-04-26 19:44:18 +0000

    Added area_perimeter.cpp , calculate area and perimeter of rectangle using C++ math operators

---
+++ area_perimeter.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Malcolm Tompkins All rights reserved
+//
+// Created by Malcolm Tompkins
+// Created on April 26, 2021
+// Finding area and perimeter using a 5cm x 3cm rectangle with proper C++ style
+#include <iostream>
+
+int main() {
+    std::cout << "Dimensions of rectangle: 5cm x 3cm\n";
+    std::cout << "Area is: " << 5*3 << "cm^2\n";
+    std::cout << "Perimeter is " << 2*(5+3) << "cm";
+}
